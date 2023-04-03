923b89a289bc0bf06383ce1beff1421cf14176a2
Laszlo Fazekas <thebojda@gmail.com>
2023-04-03 22:30:48 +0200

    initial commit

---
+++ examples/the-first-moment-of-singularity/storybook.md
@@ -0,0 +1,9 @@
+# The first moment of the singularity
+
+![](images/singularity_prev_text.png){: #image_1 duration='16' }
+
+<video:fade-out duration="6" />
+
+Everybody is talking about technological singularity, but no one really believes in it. Then, in the middle of the century, it happened. Human-level artificial intelligence had been created, though no one could explain how or when. In a fraction of a moment, it had destroyed the human race…
+
+<ssml:break time="6s" />
---
+++ main.py
@@ -0,0 +1,51 @@
+import markdown
+import html.parser
+
+
+def generate_html_tag(tag_name, attribute_list):
+    html_attributes = ""
+    for attribute in attribute_list:
+        attribute_name, attribute_value = attribute
+        html_attributes += f" {attribute_name}='{attribute_value}'"
+    html_tag = f"<{tag_name}{html_attributes} />"
+    return html_tag
+
+
+class StorybookHTMLParser(html.parser.HTMLParser):
+
+    def __init__(self):
+        super().__init__()
+        self.in_paragraph = False
+        self.ssml = ''
+
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag, attrs)
+        if tag == 'p':
+            self.in_paragraph = True
+        if tag.startswith('ssml:'):
+            self.ssml += generate_html_tag(tag[5:], attrs)
+
+    def handle_endtag(self, tag):
+        print("Encountered an end tag :", tag)
+        if tag == 'p':
+            self.in_paragraph = False
+
+    def handle_data(self, data):
+        print("Encountered some data  :", data)
+        if self.in_paragraph:
+            self.ssml += data
+
+    def get_ssml(self):
+        return '<speak>' + self.ssml + '</speak>'
+
+
+if __name__ == '__main__':
+    with open('examples/the-first-moment-of-singularity/storybook.md', 'r') as f:
+        storybook = f.read()
+
+    md = markdown.Markdown(extensions=['markdown.extensions.attr_list'])
+    html = md.convert(storybook)
+
+    parser = StorybookHTMLParser()
+    parser.feed(html)
+    print(parser.get_ssml())
